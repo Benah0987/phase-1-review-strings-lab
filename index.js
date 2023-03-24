c2a25c886a12b12b9a1c6f07b891e0beb828ca53
Benah0987 <88454033+Benah0987@users.noreply.github.com>
2023-03-24 14:45:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+// index.js
+
+//  current user as a string
+const currentUser = 'John';
+
+// welcome message using template literals
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excited welcome message using template literals and the toUpperCase() method
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define the short greeting using template literals and the substring method, and an exclamation mark
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
